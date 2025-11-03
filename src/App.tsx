8969820263a958b7066997132b69937e508cf9c5
Max Dickinson <maxsantistadickinson@gmail.com>
2025-11-03 10:34:44 -0300

    feat: create structure page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Dashboard from "./pages";
+
 function App() {
-  return <h1 className="text-3xl text-green-500">Hello World!</h1>;
+  return <Dashboard />;
 }
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard de usuários</h1>
+
+      <Button variant="contained" color="primary">
+        Adicionar usuário
+      </Button>
+    </>
+  );
+}
+
+export default Dashboard;
